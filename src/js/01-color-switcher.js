27563cb9795191bea14084896df15faf40c95313
FrancuzAlexandr <francuz_s@ukr.net>
2022-11-02 17:31:13 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+  const startBtnEl = document.querySelector("[data-start]");
+  const stopBtnEl = document.querySelector("[data-stop]");
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  let timerId = null;
+
+  startBtnEl.addEventListener("click", onStartBtnClick);
+  stopBtnEl.addEventListener("click", onStopBtnClick);
+
+  stopBtnEl.disabled = "true";
+
+ function onStartBtnClick() {
+    stopBtnEl.removeAttribute("disabled");
+    startBtnEl.disabled = "true";
+
+    timerId = setInterval(bgColorSwitch, 1000);
+ }
+
+ function onStopBtnClick() {
+    startBtnEl.removeAttribute("disabled");
+    stopBtnEl.disabled = "true";
+
+    clearInterval(timerId)
+ }
+
+function bgColorSwitch() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+
+
+
 
