3da1797e174d879e0b7d7500df408b955b575071
FrancuzAlexandr <francuz_s@ukr.net>
2022-11-04 17:48:41 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", handleSubmitBtn);
+
+function handleSubmitBtn(event) {
+  event.preventDefault();
+  let formDelay = Number(formEl.delay.value);
+  const formStep = Number(formEl.step.value);
+  const formAmount = Number(formEl.amount.value);
+  for (let i = 1; i <= formAmount; i+=1) {
+    createPromise  (i, formDelay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    formDelay += formStep;
   }
 }
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position,delay});
+      } else {
+        reject({position,delay});
+      }
+    }, delay);
+  });
+}
+
